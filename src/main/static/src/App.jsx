3d809bfbc3c938f29a1129059a032bf77aad4507
William Hester <williamehester@gmail.com>
2025-08-04 22:37:48 -0700

    Move Telemetry query logic to TelemetryPage (renamed from FuelChartPage)

--- src/main/static/src/App.jsx
+++ src/main/static/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           <NavLink className="no-style-link hamburgerMenuItem" to="/otherlaps">Other Cars' Laps</NavLink>
           <NavLink className="no-style-link hamburgerMenuItem" to="/gaps">Current Gaps</NavLink>
           <NavLink className="no-style-link hamburgerMenuItem" to="/gapchart">Gap Chart</NavLink>
-          <NavLink className="no-style-link hamburgerMenuItem" to="/fuelcharts">Fuel Charts</NavLink>
+          <NavLink className="no-style-link hamburgerMenuItem" to="/telemetry">Telemetry Charts</NavLink>
           <NavLink className="no-style-link hamburgerMenuItem" to="/trackmap">Track Map</NavLink>
         </div>
 
--- src/main/static/src/PageRoot.jsx
+++ src/main/static/src/PageRoot.jsx
@@ -4,42 +4,17 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import { ConnectRequest, QueryTelemetryRequest } from "./live_telemetry_service_pb.js";
+import { ConnectRequest } from "./live_telemetry_service_pb.js";
 import { LiveTelemetryServiceClient } from "./live_telemetry_service_grpc_web_pb.js";
 import LapLogPage from "./laplog/LapLogPage";
 import OtherCarsLapLogPage from "./laplog/OtherCarsLapLogPage";
 import GapsPage from "./gaps/GapsPage";
 // import GapChartPage from "./gapchart/GapChartPage";
-import FuelChartPage from "./fuel/FuelChartPage";
+import TelemetryPage from "./telemetry/TelemetryPage.jsx";
 import TrackMapPage from "./trackmap/TrackMapPage";
 import App from "./App";
 
-const MAX_DATA_POINTS = 3000;
-
-const ALLOWED_HZ = [0.5, 1, 3, 6, 12, 30, 60];
-
-const coerceToAllowedHz = (goal) => ALLOWED_HZ.reduce(function (prev, curr) {
-  return (Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev);
-});
-
 export default function App2() {
-  const telemetryQueries = [
-    'FuelLevel',
-    'LAP_DELTA(FuelLevel)',
-    'TrackTempCrew',
-    'Speed',
-    'LAP_DELTA(Speed)',
-  ];
-
-  const [sampleRateHz, setSampleRateHz] = useState(8);
-  const [dataRange, setDataRange] = useState({
-    min: 0,
-    max: 300,
-  });
-  const [dataWindow, setDataWindow] = useState([-1, 0]);
-  const [counter, setCounter] = useState(0);
-
-  const [telemetryData, setTelemetryData] = useState([]);
   const [gapEntries, setGapEntries] = useState([]);
   const [lapEntries, setLapEntries] = useState([]);
   const [otherCarLapEntries, setOtherCarLapEntries] = useState([]);
@@ -47,10 +22,6 @@ export default function App2() {
 
   const client = useRef(new LiveTelemetryServiceClient(`${location.origin}/api`)).current;
 
-  const timeoutRef = useRef(null);
-  const shouldClearData = useRef(false);
-  const telemetryDataBuffer = useRef([]);
-  const dataRangeBuffer = useRef(dataRange);
   const gapBuffer = useRef([]);
   const lapBuffer = useRef([]);
   const otherCarLapBuffer = useRef([]);
@@ -106,82 +77,7 @@ export default function App2() {
   }, [client]);
 
   useEffect(() => {
-    const request = new QueryTelemetryRequest();
-    request.setSampleRateHz(sampleRateHz);
-    request.setMinSessionTime(dataWindow[0]);
-    request.setMaxSessionTime(dataWindow[1]);
-    request.setQueriesList(telemetryQueries);
-
-    console.log('Querying for new data. dataStart: %s, dataEnd: %s, sampleRateHz: %s', dataWindow[0], dataWindow[1], sampleRateHz);
-    const telemetryStream = client.queryTelemetry(request, {});
-    telemetryStream.on('data', response => {
-      if (response.hasDataRanges()) {
-        const dataRanges = response.getDataRanges();
-        const updatedDataRange = {
-          min: dataRanges.getSessionTime().getMin(),
-          max: dataRanges.getSessionTime().getMax(),
-        };
-        dataRangeBuffer.current = updatedDataRange;
-      } else {
-        const telemetryData = response.getData();
-        telemetryDataBuffer.current.push(telemetryData);
-        const sessionTime = dataRangeBuffer.current;
-        dataRangeBuffer.current.max = Math.max(sessionTime.max, telemetryData.getSessionTime());
-      }
-    });
-
-    return () => {
-      telemetryStream.cancel();
-      telemetryDataBuffer.current = [];
-      shouldClearData.current = true;
-    };
-  }, [client, counter]);
-
-  useEffect(() => {
-    if (telemetryData.length === 0) {
-      // Haven't loaded any telemetry data yet. Nothing to do.
-      return;
-    }
-    const min = telemetryData[0];
-    const max = telemetryData[telemetryData.length - 1];
-
-    const newHz = coerceToAllowedHz(1000 / (dataWindow[1] - dataWindow[0]));
-
-    if (dataWindow[0] < min.getSessionTime()
-      || dataWindow[1] > max.getSessionTime()
-      || sampleRateHz != newHz) {
-      timeoutRef.current = setTimeout(() => {
-        setCounter(currentCount => currentCount + 1);
-        setSampleRateHz(newHz);
-      }, 500);
-      return () => {
-        clearTimeout(timeoutRef.current);
-      };
-    }
-  }, [dataWindow]);
-
-  useEffect(() => {
     const intervalId = setInterval(() => {
-      if (telemetryDataBuffer.current.length > 0) {
-        setTelemetryData(prev => {
-          let dataToPrepend;
-          if (shouldClearData.current) {
-            dataToPrepend = [];
-            shouldClearData.current = false;
-          } else {
-            dataToPrepend = prev;
-          }
-          const updated = [...dataToPrepend, ...telemetryDataBuffer.current].slice(-MAX_DATA_POINTS);
-          telemetryDataBuffer.current = [];
-          // TODO: Fix this. I think this needs a ref or something.
-          if (dataWindow[0] == -1 && dataWindow[1] == 0 && updated.length > 0) {
-            setDataWindow([updated[0].getSessionTime(), updated[updated.length - 1].getSessionTime()]);
-          }
-          return updated;
-        });
-        setDataRange(dataRangeBuffer.current);
-      }
-
       // Process Gaps
       if (gapBuffer.current.length > 0) {
         const lastMessage = gapBuffer.current[gapBuffer.current.length - 1];
@@ -211,7 +107,7 @@ export default function App2() {
     }, 250); // Update the UI 4 times per second. Adjust as needed.
 
     return () => clearInterval(intervalId); // Cleanup interval on unmount
-  }, [dataWindow]); // Maybe adding dataWindow here will solve it?
+  }, []);
 
   return (
     <BrowserRouter>
@@ -222,9 +118,7 @@ export default function App2() {
           <Route path="otherlaps" element={<OtherCarsLapLogPage entries={otherCarLapEntries} drivers={currentDrivers} />} />
           <Route path="gaps" element={<GapsPage entries={gapEntries} drivers={currentDrivers} />} />
           {/* <Route path="gapchart" element={<GapChartPage distances={driverDistances} drivers={currentDrivers} />} /> */}
-          <Route path="fuelcharts" element={
-            <FuelChartPage telemetryData={telemetryData} dataRange={dataRange} dataWindow={dataWindow} setDataWindow={setDataWindow} />
-          } />
+          <Route path="telemetry" element={<TelemetryPage />} />
           <Route path="trackmap" element={<TrackMapPage />} />
         </Route>
       </Routes>
