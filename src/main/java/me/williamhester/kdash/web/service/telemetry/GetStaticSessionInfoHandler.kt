1a6160ccaa3e97d0134079a5d049242686be15bf
William Hester <williamehester@gmail.com>
2025-12-13 22:24:52 -0800

    Publish car class info as static session info

--- src/main/java/me/williamhester/kdash/web/service/telemetry/GetStaticSessionInfoHandler.kt
+++ src/main/java/me/williamhester/kdash/web/service/telemetry/GetStaticSessionInfoHandler.kt
@@ -3,6 +3,7 @@ package me.williamhester.kdash.web.service.telemetry
 import io.grpc.stub.StreamObserver
 import me.williamhester.kdash.enduranceweb.proto.ConnectRequest
 import me.williamhester.kdash.enduranceweb.proto.StaticSessionInfo
+import me.williamhester.kdash.enduranceweb.proto.carClass
 import me.williamhester.kdash.enduranceweb.proto.staticSessionInfo
 import me.williamhester.kdash.web.extensions.get
 import me.williamhester.kdash.web.models.SessionKey
@@ -20,10 +21,23 @@ class GetStaticSessionInfoHandler(
     // TODO: Make this null safe
     val metadata = Store.getMetadataForSession(sessionKey)!!
 
+    val carClasses =
+      metadata["DriverInfo"]["Drivers"].listList
+        .map {
+          carClass {
+            carClassId = it["CarClassID"].value.toInt()
+            carClassShortName = it["CarClassShortName"].value
+            carClassColor = '#' + it["CarClassColor"].value.substringAfter('x')
+          }
+        }
+        .toSet()
+
     streamObserver.onNext(
       staticSessionInfo {
         driverCarIdx = metadata["DriverInfo"]["DriverCarIdx"].value.toInt()
         driverCarEstLapTime = metadata["DriverInfo"]["DriverCarEstLapTime"].value.toFloat()
+        isMulticlass = carClasses.size > 1
+        this.carClasses += carClasses
       }
     )
   }
